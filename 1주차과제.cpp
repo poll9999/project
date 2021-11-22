7397cf507d8d8f27c837fc5f10ec0cfb6349e4ff
username459712 <94822739+username459712@users.noreply.github.com>
2021-11-22 16:23:45 +0900

    c 1 week

---
+++ 1주차과제.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum;
+	sum = 0;
+
+	for (int i = 1; i <= 10; i++)
+		sum = sum + i;
+	cout << "гую╨ " << sum << endl;
+
+	return 0;
+}
